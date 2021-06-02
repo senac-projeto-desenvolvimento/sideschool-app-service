1ffb5f11af8f2b6ab2312201ca1a13dbd30a3c0a
Lucas Alves de Lacerda <lucas_lacerda@sicredi.com.br>
2021-06-02 00:46:24 -0300

    adiciona relacionamento de aluno e classe; muda estrutura das classes de response

--- src/main/kotlin/br/com/senac/sideschoolappservice/data/QuestionDataModel.kt
+++ src/main/kotlin/br/com/senac/sideschoolappservice/data/QuestionDataModel.kt
@@ -6,31 +6,7 @@ import br.com.senac.sideschoolappservice.data.entity.QuestionEntity
 import br.com.senac.sideschoolappservice.data.entity.SubjectEntity
 import java.time.LocalDateTime
 
-data class HomeworkData(val homeworkId: Int?
-                      , val description: String
-                      , val createdDate: LocalDateTime) {
 
-    companion object {
-        fun of(source: HomeworkEntity) = HomeworkData(source.homeworkId, source.description, source.createdDate)
-    }
-}
-
-data class HomeworkResponse(val homeworkId: Int?
-                            , val description: String
-                            , val questions: List<QuestionEntity>) {
-
-    companion object {
-        fun of(homework: HomeworkEntity, questions: List<QuestionEntity>) = HomeworkResponse(homework.homeworkId, homework.description, questions)
-    }
-}
-
-class SubjectData(val subjectId: Int?
-                , val title: String) {
-
-    companion object {
-        fun of(source: SubjectEntity) = SubjectData(source.subjectId, source.title)
-    }
-}
 
 class QuestionData(val questionId: Int?
                    , val description: String) {
